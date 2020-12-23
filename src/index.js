647908d9b2103a679670b450494661dad5f601ad
pcb <pcb@unit.no>
2020-12-23 09:46:09 +0100

    es6-class

--- src/index.js
+++ src/index.js
@@ -4,6 +4,98 @@ import julieHead from './assets/julie.png';
 import julieBody from './assets/body.png';
 import star from './assets/star.png';
 
+
+class MyScene extends Phaser.Scene {
+
+
+    constructor(config) {
+        super(config);
+
+        this.score = 0;
+    }
+
+    preload() {
+        this.load.image('background', background);
+        this.load.image('julie', julieHead);
+        this.load.image('body', julieBody);
+        this.load.image('star', star);
+    }
+
+
+    create() {
+        this.input.setDefaultCursor('cell');
+        this.drawLevel1();
+        this.drawHeader();
+    }
+
+    updateScore() {
+        this.scoreText.setText(`Score: ${this.score}`);
+    }
+
+    drawJulie() {
+        const julieHead = this.add.image(620, 270, 'julie').setScale(0.5, 0.5).setOrigin(0.5, 1);
+        this.tweens.addCounter({
+            from: 1,
+            to: 10,
+            duration: 1000,
+            yoyo: true,
+            repeat: -1,
+            onUpdate:  (tween) => {
+                julieHead.setAngle(tween.getValue());
+            }
+        });
+        let body = this.add.image(600, 400, 'body');
+        body.setScale(1.5, 1.5);
+
+    }
+
+    drawStars() {
+        const star1 = this.add.sprite(100, 100, "star");
+        const star2 = this.add.sprite(100, 150, "star");
+        this.tweens.add({
+            x: 600,
+            targets: [star1,star2],
+            duration: 3000,
+            yoyo: true,
+            repeat: -1
+        })
+        star1.setInteractive().on('pointerdown',  () =>{
+            star1.destroy();
+            this.score++;
+            this.updateScore();
+        });
+        star2.setInteractive().on('pointerdown',  ()=> {
+            star2.destroy();
+            this.score++;
+            this.updateScore();
+        });
+
+    }
+
+
+    drawHeader() {
+        const graphics = this.add.graphics();
+        graphics.fillStyle(0x440044, 1);
+        graphics.fillRect(0, 0, 800, 50);
+
+        this.scoreText = this.add.text(10, 10, "", {
+            fontFamily: "Arial Black",
+            fontSize: 24,
+            color: "#c51b7d",
+            align: 'center'
+        }).setStroke('#de77ae', 4);
+        this.updateScore();
+    }
+
+    drawLevel1() {
+        let background = this.add.image(400, 300, 'background');
+        background.setScale(0.7, 0.7);
+        this.drawJulie();
+        this.drawStars();
+    }
+}
+
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -20,57 +112,8 @@ const config = {
 
         }
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [MyScene]
 };
 
-function preload() {
-    this.load.image('background', background);
-    this.load.image('julie', julieHead);
-    this.load.image('body', julieBody);
-    this.load.image('star', star);
-}
-
-function create() {
-    let background = this.add.image(400, 300, 'background');
-    background.setScale(0.7, 0.7);
-    let body = this.add.image(600, 400, 'body');
-    body.setScale(1.5, 1.5);
-    const julie = this.add.image(620, 270, 'julie').setScale(0.5, 0.5).setOrigin(0.5, 1);
-    const sprite = this.add.sprite(400, 300, 'julie').setScale(0.3);
-    let star = this.add.image(100, 100, "star");
-    this.tweens.add({
-        x: 600,
-        targets: star,
-        duration: 3000,
-        yoyo: true,
-        repeat: -1
-    })
-    this.tweens.addCounter({
-        from: 1,
-        to: 10,
-        duration: 1000,
-        yoyo: true,
-        repeat: -1,
-        onUpdate: function (tween) {
-            julie.setAngle(tween.getValue());
-        }
-    });
-    sprite.setInteractive();
-    sprite.on('pointerover', function () {
-        sprite.setTint('0x00ff00');
-    });
-    sprite.on('pointerout', function () {
-        sprite.clearTint();
-    });
-}
-
-function update() {
-}
-
 const game = new Phaser.Game(config);
-game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
 
