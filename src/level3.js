a7f16065a114da4561ca796a382d5303d636bfe0
pcb <pcb@unit.no>
2020-12-30 10:09:13 +0100

    level3

--- src/level3.js
+++ src/level3.js
@@ -1,5 +1,6 @@
 import nextLevelArrow from "./assets/right-arrow.png";
 import julieHead from "./assets/julie.png";
+import cloud from "./assets/cloud.png";
 
 export default class Level3 extends Phaser.Scene {
 
@@ -8,50 +9,55 @@ export default class Level3 extends Phaser.Scene {
             key: 'Level3'
         })
         this.arrowAngle = 0;
+        this.cursors = null;
     }
 
     preload() {
         this.load.image('nextLevelArrow', nextLevelArrow);
         this.load.image('julie', julieHead);
+        this.load.image('cloud', cloud);
+        this.load.image('nextLevelArrow', nextLevelArrow);
+
     }
 
     create() {
         this.physics.world.setBoundsCollision(true, true, true, true);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.character = this.physics.add.sprite(100, 150, 'julie')
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(270, 380, 'cloud').setOrigin(0).refreshBody();
+
+        this.character = this.physics.add.sprite(380, 150, 'julie')
             .setScale(0.2)
-            .setGravityY(150)
+            .setGravityY(250)
             .setCollideWorldBounds(true)
         this.character.body.onWorldBounds = true;
+
+        this.physics.add.collider(this.character, platforms);
+
         let won = false;
 
         this.physics.world.on("worldbounds", (body) => {
             if (body.gameObject === this.character) {
-                body.gameObject.disableBody(true, true);
+                this.character.setPosition(380, 150);
             }
         });
 
     }
 
-    drawJulie() {
-        this.julie = this.physics.add.sprite(130, 500, 'julie')
-            .setScale(0.1)
-            .setFlipX(true);
-        //add animation on julie
-        this.julie.scene.tweens.add({
-            targets: this.julie,
-            x: 700,
-            angle: 1080,
-            duration: 5000,
-            ease: 'Sine.easeInOut',
-            repeat: -1,
-            yoyo: true,
-            flipX: true,
-        });
+    update() {
+        if (this.cursors) {
+            if (this.cursors.left.isDown) {
+                this.character.setVelocityX(-160);
+            } else if (this.cursors.right.isDown) {
+                this.character.setVelocityX(160);
+            } else {
+                this.character.setVelocityX(0);
+            }
+        }
     }
 
-
-    level2Win() {
+    level3Win() {
         //TODO: show happyPer
         this.add.image(700, 550, 'nextLevelArrow')
             .setScale(0.1)
