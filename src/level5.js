326e8526052d0e606c706103e47463a83e410884
pcb <pcb@unit.no>
2021-01-20 12:47:37 +0100

    level5

--- src/level5.js
+++ src/level5.js
@@ -1,13 +1,15 @@
-import nextLevelArrow from "./assets/right-arrow.png";
-import background_level5 from "./assets/background_level5.png";
-import stone from "./assets/stone_100.png";
-import stoneSmall from "./assets/stone20.png";
+import background_level5 from "./assets/background_level5.jpg";
 import balloon from "./assets/balloon.png";
-import safe_closed from "./assets/safe_closed.png";
-import safe_opened from "./assets/safe_opened.png";
+import safe_closed from "./assets/safe.png";
+import safe_opened from "./assets/safe_open.png";
+import safe_button from "./assets/safe_button.png";
 import poster1 from "./assets/poster1.png";
 import poster2 from "./assets/poster2.png";
-import wall_image from "./assets/wall_image.png";
+import poster1_teared from "./assets/poster1_teared.png";
+import picture1 from "./assets/picture1.png";
+import picture2 from "./assets/picture2.png";
+import picture3 from "./assets/picture3.png";
+import nextLevelArrowSheet from "./assets/next_level_arrows.png";
 
 export default class Level5 extends Phaser.Scene {
 
@@ -15,39 +17,44 @@ export default class Level5 extends Phaser.Scene {
         super({
             key: 'Level5'
         })
+        this.safeCombination = "5319";
+        this.pressedCombination = "";
+        this.keyPadGridText = [];
+        this.keyPadGrid = [];
+
     }
 
     preload() {
         this.load.image('background_level5', background_level5);
-        this.load.image('stone', stone);
-        this.load.image('stoneSmall', stoneSmall);
-        this.load.image('nextLevelArrow', nextLevelArrow);
+        this.load.image('safe_button', safe_button);
         this.load.image('balloon', balloon);
         this.load.image('poster1', poster1);
         this.load.image('poster2', poster2);
+        this.load.image('poster1_teared', poster1_teared);
         this.load.image('safe_closed', safe_closed);
         this.load.image('safe_opened', safe_opened);
-        this.load.image('wall_image', wall_image);
+        this.load.image('picture1', picture1);
+        this.load.image('picture2', picture2);
+        this.load.image('picture3', picture3);
+        this.load.spritesheet('nextLevelArrows', nextLevelArrowSheet, {frameWidth: 100, frameHeight: 75});
+
     }
 
     pressDigit(number) {
-        console.log(number);
+        this.pressedCombination += number;
+        console.log(this.pressedCombination);
+        if (this.pressedCombination.endsWith(this.safeCombination)) this.openSafe();
     }
 
-    create() {
-        this.header = this.scene.get('Header');
-        this.header.setLevelText("Level 5");
-        this.add.image(0, 50, 'background_level5').setOrigin(0);
-
-        this.keyPadGrid = []
-
+    drawKeyPads() {
         for (let x = 0; x < 3; x++) {
             this.keyPadGrid[x] = [];
+            this.keyPadGridText[x] = [];
             for (let y = 0; y < 3; y++) {
-                const sx = 300 + (x * 36);
-                const sy = 200 + (y * 36);
+                const sx = 355 + (x * 34);
+                const sy = 265 + (y * 34);
                 const value = x + 1 + (y * 3);
-                const block = this.add.image(sx, sy, 'stoneSmall')
+                const block = this.add.image(sx, sy, 'safe_button')
                     .setInteractive({useHandCursor: true})
                     .on('pointerover', () => {
                         block.setTint(0xbbbbbb);
@@ -67,37 +74,58 @@ export default class Level5 extends Phaser.Scene {
                     });
                 //show text
                 this.keyPadGrid[x][y] = block;
-                this.hintText = this.add.text(sx, sy, value)
+                this.keyPadGridText[x][y] = this.add.text(sx - 5, sy - 10, value)
                     .setFontSize(16)
                     .setFontFamily('Arial')
-                    .setColor('#aa4444');
+                    .setColor('#555555');
 
             }
         }
-        console.log(this.keyPadGrid);
+    }
 
-        this.poster1 = this.add.image(650, 300, 'poster1').setOrigin(0)
+    clearKeyPad() {
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                this.keyPadGrid[x][y].destroy();
+                this.keyPadGridText[x][y].destroy();
+            }
+        }
+    }
+
+    create() {
+        this.header = this.scene.get('Header');
+        this.header.setLevelText("Level 5");
+        this.add.image(0, 50, 'background_level5').setOrigin(0);
+
+
+        this.add.image(10, 190, 'poster2').setOrigin(0)
+        this.poster1 = this.add.image(670, 200, 'poster1').setOrigin(0)
             .setInteractive({useHandCursor: true})
             .on('pointerup', () => {
                 this.poster1.destroy();
-                this.add.image(650, 300, 'poster2').setOrigin(0);
+                this.add.image(647, 200, 'poster1_teared').setOrigin(0);
             });
 
-        this.wall_image = this.add.image(350, 200, 'wall_image').setOrigin(0)
+        this.add.image(160, 250, 'picture3').setOrigin(0)
+        this.add.image(475, 250, 'picture2').setOrigin(0)
+        this.wall_image = this.add.image(315, 225, 'picture1').setOrigin(0)
             .setInteractive({useHandCursor: true})
             .on('pointerup', () => {
                 this.wall_image.destroy();
-                this.safe_closed = this.add.image(350, 200, 'safe_closed').setOrigin(0)
-                    .setInteractive({useHandCursor: true})
-                    .on('pointerup', () => {
-                        this.safe_closed.destroy();
-                        this.add.image(300, 200, 'safe_opened').setOrigin(0);
-                        this.balloon = this.add.image(300, 200, 'balloon').setOrigin(0)
-                            .setInteractive({useHandCursor: true})
-                            .on('pointerup', () => {
-                                this.level5Win();
-                            });
-                    });
+                this.safe_closed = this.add.image(320, 230, 'safe_closed').setOrigin(0)
+                this.drawKeyPads();
+            });
+
+    }
+
+    openSafe() {
+        this.safe_closed.destroy();
+        this.clearKeyPad();
+        this.add.image(275, 200, 'safe_opened').setOrigin(0).setDepth(10);
+        this.balloon = this.add.image(370, 250, 'balloon').setOrigin(0).setDepth(10)
+            .setInteractive({useHandCursor: true})
+            .on('pointerup', () => {
+                this.level5Win();
             });
 
     }
@@ -105,8 +133,7 @@ export default class Level5 extends Phaser.Scene {
 
     level5Win() {
         //TODO: show happyPer
-        this.balloon.setPosition(115, 290);
-
+        // this.balloon.setPosition(115, 290);
         this.anims.create({
             key: 'blinking_arrows',
             frames: this.anims.generateFrameNumbers('nextLevelArrows', {frames: [0, 1, 2]}),
